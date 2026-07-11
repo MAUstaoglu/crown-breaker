465c9f0c041b8936b331a0cbc51dbfe45789e85a
Mehmet Ali Ustaoğlu <ali.ustaoglu@icloud.com>
2026-07-11 19:17:23 +0200

    feat: register SharedPreferencesPlugin for tvOS in GeneratedPluginRegistrant

--- tvos/Flutter/GeneratedPluginRegistrant.swift
+++ tvos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import Flutter
 import Foundation
 
+import shared_preferences_tvos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
